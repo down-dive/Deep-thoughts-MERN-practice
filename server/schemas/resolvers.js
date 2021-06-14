a7d93078e100eb44ff9ed7ca38388cd148fbc6e7
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-14 15:29:06 -0700

    models and config files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World!';
+        thoughts: async () => {
+            return Thought.find().sort({ createdAt: -1 });
         }
     }
 };
