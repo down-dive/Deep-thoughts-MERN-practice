4179e6ee0f2cb8e4078de0c7eac09d1845bef277
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-15 16:35:18 -0700

    headers to popilate me query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,10 +34,12 @@ const typeDefs = gql`
     thought(_id: ID!): Thought
   }
   
-
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 
   type Auth {
