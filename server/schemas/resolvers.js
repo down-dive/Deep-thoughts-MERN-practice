af848f1bda4806b50721b6634d25d6e8999c374a
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-15 07:25:00 -0700

    adding jwt authenticaiton

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+
 
 const resolvers = {
   Query: {
@@ -26,12 +28,24 @@ const resolvers = {
             const user = await User.create(args);
 
             return user;
-
+    
         },
-        login: async () => {
-
-        }
-    }
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            return user;
+          }
+      }
   }
 };
 
