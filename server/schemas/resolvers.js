d3e81771375fbfd7b2d936e26790a7539dd0a45f
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-15 07:28:46 -0700

    adding jwt authenticaiton

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,7 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 
 
 const resolvers = {
